705fc38c3893f71cb81c6e15a8b346030bcf8c6c
Diego Miguel Gonzalez Calero <diegomzalez@hotmail.com>
2024-10-21 15:54:28 -0700

    feat: general-metadata
    Add metadata to the main layout of the application: PAMBII as the title and a short description about this project.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 export const metadata: Metadata = {
-  title: "",
-  description: "",
+  title: "PAMBII",
+  description:
+    "pambi.tech website clone by Dalas. This is not the official website.",
 };
 export default function RootLayout({
   children,
