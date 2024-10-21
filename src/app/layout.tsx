2adcaf882e1da499e1d4dbede8d15be106f5010a
Diego Miguel Gonzalez Calero <diegomzalez@hotmail.com>
2024-10-21 16:54:36 -0700

    feat: general_layout-top-banner
    Add a top banner for the main layout of the app with its styles.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
+import { Chakra_Petch } from "next/font/google";
 export const metadata: Metadata = {
   title: "PAMBII",
   description:
     "pambi.tech website clone by Dalas. This is not the official website.",
 };
+const font_chakra = Chakra_Petch({ subsets: ["latin"], weight: "700" });
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -13,7 +15,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased">{children}</body>
+      <body className="antialiased text-white">
+        <aside className="top-banner bg-primary sticky h-[50px] flex justify-center items-center">
+          <p className={`${font_chakra.className} text-base md:text-xl`}>
+            <span className="font-semibold">
+              <Link className="underline" href={"https://pambii.quest"}>
+                Pambii Quest
+              </Link>
+              &nbsp;IS LIVE
+            </span>
+            , participate to get reward
+          </p>
+        </aside>
+        {children}
+      </body>
     </html>
   );
 }
