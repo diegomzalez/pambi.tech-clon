f6de111bf328ce8395938cdb8fad0dc44b2db58c
Diego Miguel Gonzalez Calero <diegomzalez@hotmail.com>
2024-10-21 15:44:46 -0700

    feat: project initialization
    Add project initialization. Cleaning genereted code by Next.js.

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+export const metadata: Metadata = {
+  title: "",
+  description: "",
+};
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="antialiased">{children}</body>
+    </html>
+  );
+}
